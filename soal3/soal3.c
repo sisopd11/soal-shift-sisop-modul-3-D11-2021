9e85097a391c9012054cd9ef4602395c9422890e
sisopd11 <80894892+sisopd11@users.noreply.github.com>
2021-05-20 15:42:15 +0800

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,57 +8,68 @@
 #include <ctype.h>
 #include <sys/stat.h>
 #include <stdbool.h>
-#include <dirent.h>
 
-char fileList[2048][PATH_MAX];
+
 pthread_t tid[500];
+char fileList[500][500];
 
-void getFileExt(char* fileName, char *exten)
+char toLower(char* str)
 {
+	 for(int i = 0; i<strlen(str); i++)
+            {
+                if(str[i]>64 && str[i]<91)
+		    str[i]+=32;
+            }
+}
+
+char getFileExt(char*fileName,char*extension)
+{
+    //char *stop = getFilename(extension);
     char *ext = strchr(fileName, '.');
     if (ext == NULL) {
-        strcpy(exten,"Unknown");
+        strcpy(extension,"Unknown");
     } 
     else if (ext == fileName){
-        strcpy(exten,"Hidden");
+        strcpy(extension,"Hidden");
     }
     else
     {
-        strcpy(exten,ext+1);
+        strcpy(extension,ext+1);
     }
 }
 
-bool checkExistAndRegFile(char *basePath)
+bool checkFile(char *basePath)
 {
     struct stat buffer;
     int exist = stat(basePath,&buffer);
     if(exist == 0)
     {
-        if( S_ISREG(buffer.st_mode) ) return true;
-        else return false;
+        if(S_ISREG(buffer.st_mode)) 
+           return true;
+        else 
+	   return false;
     }
     else  
         return false;
 }
 
-void *moveFile( void *arg )
+void *moveFile(void *arg)
 {
     char basePath[PATH_MAX];
     strcpy(basePath,(char *) arg);
 
-    if(checkExistAndRegFile(basePath))
+    if(checkFile(basePath))
     {
-        //printf("%s\n",(char*)arg);
-        const char *p="/";
-        char *a,*b;
+        char *i,*b;
         char fullPath[PATH_MAX];
         strcpy(fullPath,(char *) arg);
 
         char fileName[100];
 
-        for( a=strtok_r(fullPath,p,&b) ; a!=NULL ; a=strtok_r(NULL,p,&b) ) {
+        for(i=strtok_r(fullPath,"/",&b); i!=NULL; i=strtok_r(NULL,"/",&b)) 
+	{
             memset(fileName,0,sizeof(fileName));
-            strcpy(fileName,a);
+            strcpy(fileName,i);
         }
 
         char ext[PATH_MAX];
@@ -65,30 +77,33 @@ void *moveFile( void *arg )
 
         if(strcmp(ext,"Hidden") != 0 && strcmp(ext,"Unknown") != 0)
         {
-            for(int i = 0; i<strlen(ext); i++)
-            {
-                ext[i] = tolower(ext[i]);
-            }
+            toLower(ext);
         }
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        char workDir[PATH_MAX];
+        if (getcwd(workDir, sizeof(workDir)) == NULL)
+	{
             perror("getcwd() error");
             return (void *) 0;
         }
 
-        char destinationDir[PATH_MAX];
-        sprintf(destinationDir,"%s/%s",cwd,ext);
-        mkdir(destinationDir,0777);
+       
+        char destDir[PATH_MAX];
+        sprintf(destDir,"%s/%s",workDir,ext);
+        
+        mkdir(destDir,0777);
+
         char destination[PATH_MAX];
-        sprintf(destination,"%s/%s/%s",cwd,ext,fileName);
-	//same as move 
+        sprintf(destination,"%s/%s/%s",workDir,ext,fileName);
+       
         rename(basePath,destination);
+        
         return (void *) 1;
     }
+
     else return (void *) 0;
 }
-//template modul2 cek file in directory
-int listFilesRecursively(char *basePath, int *fileCount)
+//template modul2, cek file in directory
+int listFilesRecursively(char *basePath, int *count)
 {
     char path[PATH_MAX];
     struct dirent *dp;
@@ -103,18 +118,17 @@ int listFilesRecursively(char *basePath, int *fileCount)
         {
             char fullPath[PATH_MAX];
             sprintf(fullPath,"%s/%s",basePath,dp->d_name);
-            //printf("%s\n", fullPath);
-            if(checkExistAndRegFile(fullPath))
+            if(checkFile(fullPath))
             {
-                sprintf(fileList[*fileCount],"%s",fullPath);
-                *fileCount += 1;
+                sprintf(fileList[*count],"%s",fullPath);
+                *count = *count + 1;
             }
             // Construct new path from our base path
             strcpy(path, basePath);
             strcat(path, "/");
             strcat(path, dp->d_name);
 
-            listFilesRecursively(path,fileCount);
+            listFilesRecursively(path,count);
         }
     }
 
@@ -134,7 +148,7 @@ int main(int argc,char* argv[])
 
 		while(argv[i]!=NULL)
 		{
-		    pthread_create( &(tid[i-2]), NULL, moveFile, (void*) argv[i]);
+		    pthread_create(&(tid[i-2]), NULL, moveFile, (void*) argv[i]);
 		    i++;
 		}
 
@@ -144,29 +158,15 @@ int main(int argc,char* argv[])
 		    void *ptr;
 		    pthread_join( tid[j], &ptr);
 		    cek = (long) ptr;
-		    if(cek) printf("File %d : Berhasil Dikategorikan\n", j+1);
-		    else printf("File %d : Sad, gagal :(\n", j+1);
+		    if(cek) 
+			printf("File %d : Berhasil Dikategorikan\n", j+1);
+		    else 
+			printf("File %d : Sad, gagal :(\n", j+1);
 		}
         
         return 0;
     }
-   else if(!strcmp(argv[1],"*"))
-    {
-	int count_file=0;
-        getcwd(baseDir,sizeof(baseDir));
-	int cekFile = listFilesRecursively(baseDir, &count_file);
-    	for(int i = 0; i<count_file; i++)
-    	 {
-        	pthread_create( &(tid[i]), NULL, moveFile, (void*) fileList[i]);
-   	 }
 
-   	 for (int i = 0; i < count_file; i++)
-    	{
-        	void *ptr;
-        	pthread_join( tid[i], &ptr);
-    	}
-
-    }
 
     else if(strcmp(argv[1],"-d")==0) 
     {
@@ -194,6 +194,23 @@ int main(int argc,char* argv[])
 		
     		printf("Direktori sukses disimpan!\n");
     	}
+    }
+    else if(strcmp(argv[1],"*")==0)
+    {
+	int count_file=0;
+        getcwd(baseDir,sizeof(baseDir));
+	int cekFile = listFilesRecursively(baseDir, &count_file);
+    	for(int i = 0; i<count_file; i++)
+    	 {
+        	pthread_create( &(tid[i]), NULL, moveFile, (void*) fileList[i]);
+   	 }
+
+   	 for (int i = 0; i < count_file; i++)
+    	{
+        	void *ptr;
+        	pthread_join( tid[i], &ptr);
+    	}
+
     }
 
     return 0;
