066939c126c3f4e2cf4549e1d44f397226222760
Afifah Nur Sabrina <75364000+afifahsabrina@users.noreply.github.com>
2021-05-22 15:38:46 +0700

    Update input matrix
    Manually with function input_matrix

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <sys/ipc.h>
+#include <stdlib.h>
 
 // declaration
 int res = 0;
@@ -14,51 +15,28 @@ pthread_t thread, thread2, thread3; // create thread
 
 // first matrix function
 void *input_matrixA(void *arg) {
-   matrixA[0][0]=1; 
-   matrixA[0][1]=1; 
-   matrixA[0][2]=1;
-
-   matrixA[1][0]=1; 
-   matrixA[1][1]=2; 
-   matrixA[1][2]=1;
-
-   matrixA[2][0]=1; 
-   matrixA[2][1]=1; 
-   matrixA[2][2]=3;
-   
-   matrixA[3][0]=4; 
-   matrixA[3][1]=1; 
-   matrixA[3][2]=1;
-   return NULL;
+   printf("Matrix A:\n");
+   int i, k;
+   for(i = 0; i < 4; i++) {
+      for(k = 0; k < 3; k++) {
+         scanf("%d", &matrixA[i][k]);
+      }
+   }
 }
 
 // second matrix function
-void *input_matrixB(void *arg){ 
-   matrixB[0][0]=1; 
-   matrixB[0][1]=2; 
-   matrixB[0][2]=3; 
-   matrixB[0][3]=4; 
-   matrixB[0][4]=1; 
-   matrixB[0][5]=2; 
-
-   matrixB[1][0]=2; 
-   matrixB[1][1]=3; 
-   matrixB[1][2]=4; 
-   matrixB[1][3]=1; 
-   matrixB[1][4]=2; 
-   matrixB[1][5]=3; 
-
-   matrixB[2][0]=3; 
-   matrixB[2][1]=4; 
-   matrixB[2][2]=1;
-   matrixB[2][3]=2; 
-   matrixB[2][4]=3; 
-   matrixB[2][5]=4; 
-   return NULL;
+void *input_matrixB(void *arg) { 
+   printf("Matrix B:\n");
+   int k, j;
+   for(k = 0; k < 3; k++) {
+      for(j = 0; j < 6; j++) {
+         scanf("%d", &matrixB[k][j]);
+      }
+   }
 }
 
 // matrix multiplication function
-void *multiply(void *arg){
+void *multiply(void *arg) {
    for(int i = 0; i < 4; i++) {
       for(int j = 0;j < 6; j++) {
          for(int k = 0; k < 3; k++) {
@@ -83,8 +61,8 @@ int main(){
    pthread_create(&thread2, NULL, input_matrixB, NULL); // create thread2 for matrixB
    pthread_join(thread2,NULL);
 
-   for(int i = 0; i < 4; i++){ // set 4 rows
-      for(int j = 0; j < 6; j++){ // set 5 columns
+   for(int i = 0; i < 4; i++) { // set 4 rows
+      for(int j = 0; j < 6; j++) { // set 6 columns
          value[i][j] = 0;
       }
       pthread_create(&thread3, NULL, multiply, NULL); // create thread3 for the final matrix
@@ -92,11 +70,27 @@ int main(){
    }
 
    // print the final matrix
-   printf("Matrix: \n");
-   for(int i = 0; i < 4; i++){ // set 4 rows
-      for(int j = 0;j < 6;j++){  // set 5 columns
+   printf("The Final Matrix: \n");
+   for(int i = 0; i < 4; i++) { // set 4 rows
+      for(int j = 0;j < 6;j++) {  // set 6 columns
          printf("%d\t", value[i][j]); // print the number with a tab
       }
       printf("\n");
    }
 }
+
+/*
+4 1 4
+2 1 3
+4 2 2
+1 1 4
+Matrix B:
+2 1 3 2 0 3
+1 4 4 0 0 2
+1 1 0 1 2 1
+Final Matrix:
+13	12	16	12	8	18	
+8	9	10	7	6	11	
+12	14	20	10	4	18	
+7	9	7	6	8	9	
+*/
